80e21220321727eae60341e3d6991f50e5f305c7
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2026-04-05 17:03:54 +0300

    add client and print Vector List

--- Bank/main.cpp
+++ Bank/main.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
+struct Client{
+    string id;
+    string name;
+    string phone;
+    double balance;
+};
+
+//method declarations
 int displayMenu();
+void addClient(vector<Client>& clients);
+void getClientInfo(Client& client);
+void printVector(vector<Client> clients);
+
 int main(){
-    int choice = displayMenu();
+    vector <Client> clients = vector<Client>();
+    string fileName = "clients.txt";
     
+    int choice = displayMenu();
     switch(choice) {
         case 1: 
                 cout<<"====================================\n";
                 cout<<"VIEW CLIENT LIST"<< endl;
+                printVector(clients);
                 break;
         case 2:
+                cout<<"====================================\n";
+                cout<<"ADD NEW CLIENT"<< endl;
+                addClient(clients);
+                break;
         case 3:
         case 4:
         case 5:
@@ -44,4 +64,41 @@ int displayMenu(){
     }
     
     return choice;
+}
+
+void addClient(vector<Client>& clients) {
+    Client client;
+    getClientInfo(client);
+    clients.push_back(client);
+    cout<<"Client added to vector\n";
+    printVector(clients);
+}
+void getClientInfo(Client& client){
+    cout<<"Client Info \n";
+    cout<<"ID: ";
+    getline(cin>>ws, client.id);
+    cout<<"Name: ";
+    getline(cin, client.name);
+    cout<<"Phone: ";
+    getline(cin, client.phone);
+    cout<<"Balance: ";
+    cin >> client.balance;
+}
+
+void printVector(vector<Client> clients) {
+    cout<<"====================================\n";
+    cout<<"Clients List \n";
+    cout<<"====================================\n";
+    if (clients.size() == 0) {
+        cout <<"No Added Clients\n";
+        return;
+    }
+    for (Client& client : clients) {
+        cout << client.id << " - " << client.name << " - " << client.balance << endl;
+    }
+}
+
+void writeToFile(string fileName, vector<Client> clients) {
+    fstream File (fileName);
+
 }
